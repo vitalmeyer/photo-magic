be2586f4c501ce54219352fa2b1951f1abb95d8b
vitalmeyer <vital.meyer@gmail.com>
2018-08-18 12:56:09 +0200

    - another try to commit to remote...

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-PHOTO_FOLDER_PATH = "static/photos/"
+PHOTO_FOLDER_PATH = "static/photo-01/"
+PHOTO_FOLDER_PATH = "static/photo-02/"
+
 GOOGLE_API_KEY = "AIzaSyCr5Ne6HCgpaaVpb3L12wxcaGutythUn8A"
 #GOOGLE_API_KEY = "wrong"
